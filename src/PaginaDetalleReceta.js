ccefb716473cf2aedbf2cddb8f44083cefd10ef5
miguel <miguelpayet@gmail.com>
2017-11-02 13:35:59 -0500

    reset

--- src/PaginaDetalleReceta.js
+++ src/PaginaDetalleReceta.js
@@ -9,16 +9,20 @@ class PaginaDetalleReceta extends React.Component {
 
     render() {
         const r = this.props.estado().receta;
-        return  <div className="PaginaDetalleReceta">
-                    <h2>{r.nombre}</h2>
-                    <div><p>{r.descripcion}</p></div>
-                    <div>
-                        <ul>
-                            {r.ingredientes.map(i => <li id={i}>{i}</li>)}
-                        </ul>
-                    </div>
-                    <div dangerouslySetInnerHTML={{__html: r.instrucciones}}></div>
-                </div>
+        return <div className="PaginaDetalleReceta">
+            <h1>{r.nombre}</h1>
+            <div className="descripcion"><p>{r.descripcion}</p></div>
+            <div className="ingredientes">
+                <h2>ingredientes</h2>
+                <ul>
+                    {r.ingredientes.map(i => <li id={i}>{i}</li>)}
+                </ul>
+            </div>
+            <div className="instrucciones">
+                <h2>instrucciones</h2>
+                <div dangerouslySetInnerHTML={{ __html: r.instrucciones }}></div>
+            </div>
+        </div>
     }
 
 }
